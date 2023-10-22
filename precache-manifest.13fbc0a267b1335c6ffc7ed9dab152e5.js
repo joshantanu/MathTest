78386bce82d7a32998aee9a573dbc09b9055c042
Joshi <154171@cognizant.com>
2023-10-22 10:56:15 -0500

    added difficulty levels

---
+++ precache-manifest.13fbc0a267b1335c6ffc7ed9dab152e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcc99d4ba428d10f049b86429758f4ee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2b72afbbcdb64895dd0f",
+    "url": "/static/css/main.5b067294.chunk.css"
+  },
+  {
+    "revision": "028bd339cdc18ddf5c54",
+    "url": "/static/js/2.95649992.chunk.js"
+  },
+  {
+    "revision": "c563299317d52c1696cd",
+    "url": "/static/js/3.937f6cc8.chunk.js"
+  },
+  {
+    "revision": "2b72afbbcdb64895dd0f",
+    "url": "/static/js/main.50769247.chunk.js"
+  },
+  {
+    "revision": "037431c4b892b05fcb8c",
+    "url": "/static/js/runtime~main.2010c6ad.js"
+  }
+]);
